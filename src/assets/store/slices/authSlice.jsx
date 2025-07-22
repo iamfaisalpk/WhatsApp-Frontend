ba0cd1f5057fd6a77b267aaeb952163d7bebaabb
iamfaisalpk <iamfaizy123@gmail.com>
2025-07-22 15:43:44 +0530

    fissdsd

--- src/assets/store/slices/authSlice.jsx
+++ src/assets/store/slices/authSlice.jsx
@@ -1,7 +1,9 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import socket from "../../../../utils/socket";
 const baseURL = import.meta.env.VITE_API_URL;
-console.log('🔍 Base URL from env:', baseURL)
+console.log('🔍 Base URL from env:', baseURL);
+console.log('🔍 Full environment:', import.meta.env);
+
 
 const tokenFromStorage = localStorage.getItem("authToken");
 const userFromStorage = JSON.parse(localStorage.getItem("user")) || null;
