d2c65b062b4cefa6b09d1460bb2dfe1ae3bd6b69
iamfaisalpk <iamfaizy123@gmail.com>
2026-02-26 11:18:31 +0530

    add useMemo

--- src/assets/Components/chat/ChatList.jsx
+++ src/assets/Components/chat/ChatList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import {
